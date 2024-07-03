02380f026e4eff023308a44063be65c3ccb0a2e0
AnshikaPand <168203942+AnshikaPand@users.noreply.github.com>
2024-07-03 12:29:57 +0530

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var options = {
+        strings: ["Frontend Developer", "Student", "Learner"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    };
+
+    var typed = new Typed('.multiple-text', options);
+});
